00ed484303ae760e0aba3658c6c1ceda4579dbd9
User <User@User-PC>
2021-03-17 12:23:58 -0300

    Segundo comit

--- TrabajoPractico1/src/game2048/CodigoNegocio.java
+++ TrabajoPractico1/src/game2048/CodigoNegocio.java
@@ -1,10 +1,95 @@
 package game2048;
 
-public class CodigoNegocio {
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
-	public static void main(String[] args) {
+public class CodigoNegocio 
+{
+	int [][] matriz;
+	boolean [][] matrizBoolean;
+	
+	
+	public CodigoNegocio() 
+	{
+		matriz = new int [4][4];
+		matrizBoolean = new boolean [4][4];
+	       
+	}
+	
+	public ArrayList<Tupla<Integer,Integer>> lugaresVacios(boolean[][] m)
+	{
+		ArrayList <Tupla<Integer,Integer>> guarda = new ArrayList<Tupla<Integer,Integer>>();
+		for (int i = 0 ; i< m.length ; i++)
+			for (int j = 0 ; j< m.length ; j++)
+				if (m[i][j] == false)
+					guarda.add(new Tupla<Integer,Integer>(i,j));
+		
+		return guarda;
+	}
+	
+	public void ocuparLugar (int[][]mtx,boolean[][]bmtx)
+	{
+		List<Integer> lista = Arrays.asList(2, 4);
+        Random aleatorio = new Random();
+        int Dos_o_Cuatro = lista.get(aleatorio.nextInt(lista.size()));
+        
+        Tupla<Integer,Integer> lugarElegido =lugaresVacios(bmtx).get(aleatorio.nextInt());
+        mtx[lugarElegido.x][lugarElegido.y] = Dos_o_Cuatro;
+        bmtx[lugarElegido.x][lugarElegido.y] = true;
+	}
+	
+	public int dos_o_Cuatro() 
+	{
+		List<Integer> lista = Arrays.asList(2, 4);
+        Random aleatorio = new Random();
+        int Dos_o_Cuatro = lista.get(aleatorio.nextInt(lista.size()));
+        
+        return Dos_o_Cuatro;
+	}
+	
+	
+	public void keyTyped(KeyEvent event) 
+	{
+		if (event.getKeyCode() == KeyEvent.VK_UP) {
+			moverTodoArriba();
+	    }
+		if (event.getKeyCode() == KeyEvent.VK_DOWN) {
+			moverTodoAbajo();
+		}
+		if (event.getKeyCode() == KeyEvent.VK_LEFT) {
+			moverTodoIzquierda();
+		}
+		if (event.getKeyCode() == KeyEvent.VK_RIGHT) {
+			moverTodoDerecha();
+		}
+	}
+	
+	private void moverTodoDerecha() {
 		// TODO Auto-generated method stub
+		
+	}
 
+	private void moverTodoIzquierda() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void moverTodoAbajo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void moverTodoArriba() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		
 	}
 
 }
--- TrabajoPractico1/src/game2048/InterfazDeJuego.java
+++ TrabajoPractico1/src/game2048/InterfazDeJuego.java
@@ -3,10 +3,29 @@ package game2048;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.JTable;
 
 public class InterfazDeJuego {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
+	private JTextField textField_14;
+	private JTextField textField_15;
 
 	/**
 	 * Launch the application.
@@ -28,7 +47,18 @@ public class InterfazDeJuego {
 	 * Create the application.
 	 */
 	public InterfazDeJuego() {
+		try 
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} 
+		catch (Exception e) 
+		{
+
+			e.printStackTrace();
+		}
+	
 		initialize();
+		
 	}
 
 	/**
@@ -38,6 +68,107 @@ public class InterfazDeJuego {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		CodigoNegocio MatrizPrincipal = new CodigoNegocio();
+			
+		textField = new JTextField("2");
+		textField.setEditable(false);
+		textField.setBounds(34, 49, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField(Integer.toString(MatrizPrincipal.dos_o_Cuatro()));
+		textField_1.setEditable(false);
+		textField_1.setBounds(130, 49, 86, 20);
+		textField_1.setColumns(10);
+		frame.getContentPane().add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setBounds(226, 49, 86, 20);
+		textField_2.setColumns(10);
+		frame.getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setEditable(false);
+		textField_3.setBounds(322, 49, 86, 20);
+		textField_3.setColumns(10);
+		frame.getContentPane().add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setEditable(false);
+		textField_4.setBounds(34, 80, 86, 20);
+		textField_4.setColumns(10);
+		frame.getContentPane().add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setEditable(false);
+		textField_5.setBounds(34, 111, 86, 20);
+		textField_5.setColumns(10);
+		frame.getContentPane().add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setEditable(false);
+		textField_6.setBounds(34, 142, 86, 20);
+		textField_6.setColumns(10);
+		frame.getContentPane().add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setEditable(false);
+		textField_7.setBounds(130, 80, 86, 20);
+		textField_7.setColumns(10);
+		frame.getContentPane().add(textField_7);
+		
+		textField_8 = new JTextField();
+		textField_8.setEditable(false);
+		textField_8.setBounds(130, 111, 86, 20);
+		textField_8.setColumns(10);
+		frame.getContentPane().add(textField_8);
+		
+		textField_9 = new JTextField();
+		textField_9.setEditable(false);
+		textField_9.setBounds(130, 142, 86, 20);
+		textField_9.setColumns(10);
+		frame.getContentPane().add(textField_9);
+		
+		textField_10 = new JTextField();
+		textField_10.setEditable(false);
+		textField_10.setBounds(226, 80, 86, 20);
+		textField_10.setColumns(10);
+		frame.getContentPane().add(textField_10);
+		
+		textField_11 = new JTextField();
+		textField_11.setEditable(false);
+		textField_11.setBounds(226, 111, 86, 20);
+		textField_11.setColumns(10);
+		frame.getContentPane().add(textField_11);
+		
+		textField_12 = new JTextField();
+		textField_12.setEditable(false);
+		textField_12.setBounds(226, 142, 86, 20);
+		textField_12.setColumns(10);
+		frame.getContentPane().add(textField_12);
+		
+		textField_13 = new JTextField();
+		textField_13.setEditable(false);
+		textField_13.setBounds(322, 80, 86, 20);
+		textField_13.setColumns(10);
+		frame.getContentPane().add(textField_13);
+		
+		textField_14 = new JTextField();
+		textField_14.setEditable(false);
+		textField_14.setBounds(322, 111, 86, 20);
+		textField_14.setColumns(10);
+		frame.getContentPane().add(textField_14);
+		
+		textField_15 = new JTextField();
+		textField_15.setEditable(false);
+		textField_15.setBounds(322, 142, 86, 20);
+		textField_15.setColumns(10);
+		frame.getContentPane().add(textField_15);
+		
+		
+		
 	}
-
 }
